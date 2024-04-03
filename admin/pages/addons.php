fc7bb444a380cd51d3892801f23c1dc1bd1ed6b8
bikendi-tech-solutions <bikenditechsolutions@gmail.com>
2024-04-03 23:05:01 +0100

    update on the addons

--- admin/pages/addons.php
+++ admin/pages/addons.php
@@ -158,7 +158,7 @@ $version = $path["Version"];
 		
 		foreach($file as $key => $value){
 			
-	if(is_vp_plugin_installed($value['slug'])){
+	if(!is_vp_plugin_installed($value['slug'])){
 				$installed = true;
 			$path = ABSPATH."/wp-content/plugins/".$value["slug"];
 			$path = get_plugin_data($path);
@@ -176,21 +176,13 @@ $version = $path["Version"];
             <td><?php echo $version;?></td>
             <td><a href="<?php echo $value['documentation'];?>">Link</a> </td>
             <td>
-			<?php
-			
-			if($installed == false && (vp_getoption($value['require']) == "yes" || vp_getoption($value['require']) == "false")){
-				?>
+
 				<form targert="_self" method="post" class="do_this<?php echo $key;?>">
 				<input type="text" class="btn btn-danger install visually-hidden link<?php echo $key;?>" value="<?php echo $value['plugin_link'];?>" name="link">
 				<input type="text" class="btn btn-danger install visually-hidden slug<?php echo $key;?>" value="<?php echo $value['slug'];?>" name="slug">
 				<input type="button" class="btn btn-danger install vpaction<?php echo $key;?>" value="Install" name="vpaction">
 				</form>
-				<?php
-              
-			}
-			 
-			 ?>
-
+        
             </td>		
 					<script>
 jQuery(document).ready(function(){jQuery("#cover-spin").hide()});
